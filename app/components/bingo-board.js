046457f73c5b0ed8acf971a9683ce770f44ff254
allthesignals <wilburnforce@gmail.com>
2020-09-10 14:47:51 -0400

    Add static test data

--- app/components/bingo-board.js
+++ app/components/bingo-board.js
@@ -1,11 +1,6 @@
 import Component from '@ember/component';
-import { computed } from '@ember/object';
 import { mapBy, and, or, alias } from '@ember/object/computed';
 import { inject as service } from '@ember/service';
-
-const ROW_SIZE = 5;
-const COLUMN_SIZE = 5;
-const DIAGONAL_SIZE = 5;
 
 export default Component.extend({
   classNames: ['bingo-board'],
@@ -22,99 +17,14 @@ export default Component.extend({
   tiles: alias('tilesAdapter.tiles'),
   tilesSelected: mapBy('tiles', 'selected'),
 
-  win: or('_winByRow', '_winByColumn', '_winByDiagonal'),
-  showWin: false,
-
-  shouldShowWin: and('win', 'showWin'),
-
-  _winByRow: computed('tilesSelected', {
-    get() {
-      const tilesSelected = this.get('tilesSelected');
-      let bingo = 0;
-      let index = 0;
-
-      while (index < tilesSelected.length) {
-        if (tilesSelected[index]) {
-          bingo += 1;
-          if (bingo === ROW_SIZE) {
-            return true;
-          }
-          index += 1;
-        } else {
-          bingo = 0;
-          // set index to beginning of next row
-          index = (Math.floor(index / ROW_SIZE) + 1) * ROW_SIZE;
-        }
-      }
-      return false;
-    }
-  }),
-
-  _winByColumn: computed('tilesSelected', {
-    get() {
-      const tilesSelected = this.get('tilesSelected');
-      let bingo = 0;
-      let index = 0;
-
-      while (index < tilesSelected.length) {
-        if (tilesSelected[index]) {
-          bingo += 1;
-          if (bingo === COLUMN_SIZE) {
-            return true;
-          }
-          index += COLUMN_SIZE;
-        } else {
-          bingo = 0;
-          const columnIndex = index % COLUMN_SIZE;
-
-          if (columnIndex === COLUMN_SIZE - 1) {
-            return false;
-          }
-          // set index to beginning of next column
-          index = columnIndex + 1;
-        }
-      }
-    }
-  }),
-
-  _winByDiagonal: computed('tilesSelected', {
-    get() {
-      const tilesSelected = this.get('tilesSelected');
-      const diagonalIndices = [
-        [0, 6, 12, 18, 24],
-        [4, 8, 12, 16, 20]
-      ]
-      let bingo = 0;
-
-      for (let i = 0; i < diagonalIndices.length; i++) {
-        const indices = diagonalIndices[i];
-
-        for (let j = 0; j < indices.length; j++) {
-          const diagonalIndex = indices[j];
-
-          if (tilesSelected[diagonalIndex]) {
-            bingo += 1;
-          } else {
-            bingo = 0;
-            break;
-          }
-        }
-        if (bingo === DIAGONAL_SIZE) {
-          return true;
-        }
-      }
-      return false;
-    }
-  }),
-
   actions: {
     toggleTileSelect(tile) {
       const selected = tile.get('selected');
       tile.set('selected', !selected);
 
-      this.get('tilesAdapter').saveTiles();
+      this.tilesAdapter.saveTiles();
 
-      const win = this.get('win');
+      const win = this.win;
       this.set('showWin', win);
     }
   }
--- app/utils/sponsors.js
+++ app/utils/sponsors.js
@@ -2,35 +2,207 @@ function randomNumber(max) {
   return Math.floor(Math.random() * max)
 }
 
-const TEST_SPONSORS = [
-  {
-    name: 'Apple',
-    tier: 'diamond',
-    activity: {
-      title: 'Do a thing',
-      description: 'Take a photo by a thing',
-    },
-  },
-  ...(generateFakeSponsors('bronze', 12, ['The Mule', 'The Boom', 'Sonic'])),
-  ...(generateFakeSponsors('silver', 8, ['Loves', 'QuickTrip', 'Baseball'])),
-  ...(generateFakeSponsors('gold', 4, ['QuickEMart', 'Pear'])),
-];
-
-function generateFakeSponsors(tier, count, names = ['']) {
-  const randomName = () => names[randomNumber(names.length)];
-
-  return Array.from({ length: count }).map(() => ({
-    name: randomName(),
-    tier: tier,
-    activity: {
-      title: 'Do a thing',
-      description: 'Take a photo by a thing',
-    },
-  }));
-}
-
 const SPONSORS = [
-  ...TEST_SPONSORS, // replace with real data later!
+  {
+    tier: 'diamond',
+    name: 'B&E Pest Control',
+    activity: {
+      title: 'water 4 sisu',
+      description: 'bring a case of bottled water to sisu, take a selfie'
+    }
+  },
+  {
+    tier: 'gold',
+    name: 'CACI',
+    activity: {
+      title: 'plaza roll call',
+      description: 'post a selfie in front of dig it in the plaza to your story, take a screen shot'
+    }
+  },
+  {
+    tier: 'gold',
+    name: 'Old Republic Title',
+    activity: {
+      title: 'this is what democracy looks like',
+      description: 'write a letter to an elected official encouraging them to fund homeless services'
+    }
+  },
+  {
+    tier: 'gold',
+    name: 'Supreme Lending',
+    activity: {
+      title: 'Capitol Hill hello',
+      description: 'Take a selfie outside of 1984 studios, post it to your story, take a screenshot'
+    }
+  },
+  {
+    tier: 'gold',
+    name: 'Cafe 501',
+    activity: {
+      title: 'curbside collab',
+      description: 'buy a curbside chronicle, take a selfie'
+    }
+  },
+  {
+    tier: 'silver',
+    name: "Eileen's Cookies",
+    activity: {
+      title: 'all aboard',
+      description: 'take a selfie featuring the train depot mural'
+    }
+  },
+  {
+    tier: 'silver',
+    name: 'Hefner Grill',
+    activity: {
+      title: 'pick me up',
+      description: 'write an encouraging letter to a homeless youth, drop it off at sisu'
+    }
+  },
+  {
+    tier: 'silver',
+    name: 'OCD Cleaning',
+    activity: {
+      title: 'dont be an ass',
+      description: 'like the mule on facebook, take a screenshot'
+    }
+  },
+  {
+    tier: 'silver',
+    name: 'Rustic Cuff',
+    activity: {
+      title: 'Do the okie pokie',
+      description: 'do the hokey pokey on the steps of the state capital, post the video to your story, tag sisu'
+    }
+  },
+  {
+    tier: 'silver',
+    name: 'Salata',
+    activity: {
+      title: 'stronger together',
+      description: 'buy a sisu stronger together shirt'
+    }
+  },
+  {
+    tier: 'silver',
+    name: 'Lifetime Athletics',
+    activity: {
+      title: 'captain underpants',
+      description: 'bring new underwear to sisu, socks count, no tighty whities'
+    }
+  },
+  {
+    tier: 'silver',
+    name: '3Sixty',
+    activity: {
+      title: "Now it's time to get funky",
+      description: 'take a selfie at factory obscura mix tape'
+    }
+  },
+  {
+    tier: 'silver',
+    name: "Aunti Em's Sweets",
+    activity: {
+      title: 'wanderlust',
+      description: 'check out aqueous at oklahoma contemporary, take a selfie'
+    }
+  },
+  {
+    tier: 'bronze',
+    name: 'Barking Dog Bakery',
+    activity: {
+      title: 'winter is coming',
+      description: 'bring a new or lightly used coat to sisu'
+    }
+  },
+  {
+    tier: 'bronze',
+    name: 'CMG Financial - Lisa Pape',
+    activity: {
+      title: 'Slide to the left',
+      description: ''
+    }
+  },
+  {
+    tier: 'bronze',
+    name: 'Elliot Roofing',
+    activity: {
+      title: 'Slide to the right',
+      description: ''
+    }
+  },
+  {
+    tier: 'bronze',
+    name: 'Embassy Suites - Norman',
+    activity: {
+      title: 'Criss cross',
+      description: ''
+    }
+  },
+  {
+    tier: 'bronze',
+    name: 'Firebirds - OKC',
+    activity: {
+      title: 'Cha cha now yall',
+      description: ''
+    }
+  },
+  {
+    tier: 'bronze',
+    name: 'Jan Wysocki - personal donation',
+    activity: {
+      title: 'Hands on your knees',
+      description: ''
+    }
+  },
+  {
+    tier: 'bronze',
+    name: 'Jana Thompson',
+    activity: {
+      title: 'Reverse, reverse',
+      description: ''
+    }
+  },
+  {
+    tier: 'bronze',
+    name: 'Kendra Scott',
+    activity: {
+      title: 'Charlie brown',
+      description: ''
+    }
+  },
+  {
+    tier: 'bronze',
+    name: 'Nothing Bundt Cakes',
+    activity: {
+      title: 'Two hops now yall',
+      description: ''
+    }
+  },
+  {
+    tier: 'bronze',
+    name: 'Oklahoma Fidelity Bank',
+    activity: {
+      title: 'Hands on your knees',
+      description: ''
+    }
+  },
+  {
+    tier: 'bronze',
+    name: "Packard's",
+    activity: {
+      title: "Drop it like it's hot",
+      description: ''
+    }
+  },
+  {
+    tier: 'bronze',
+    name: "Rudy's BBQ",
+    activity: {
+      title: 'Pop lock and drop it',
+      description: ''
+    }
+  },
 ];
 
 // tmp
