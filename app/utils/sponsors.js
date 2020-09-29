6e77d4b89e2de4b121f76b337add31c7f56b9766
allthesignals <wilburnforce@gmail.com>
2020-09-29 19:28:19 -0400

    Update sponsors

--- app/utils/sponsors.js
+++ app/utils/sponsors.js
@@ -6,230 +6,197 @@ function randomNumber(max) {
 
 const SPONSORS = [
   {
-    tier: 'diamond',
-    name: 'B&E Pest Control',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'water 4 sisu',
-      description: 'bring a case of bottled water to sisu, take a selfie'
+    "tier": "diamond",
+    "name": "Inasmuch Foundation",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "2020's Viva Las Sisu Treasure Hunt is proudly presented by the Inasmuch Foundation."
     }
   },
   {
-    tier: 'gold',
-    name: 'CACI',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'plaza roll call',
-      description: 'post a selfie in front of dig it in the plaza to your story, take a screen shot'
+    "tier": "gold",
+    "name": "Haskell",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Stop into Not Your Average Joe and tag yourself & the Viva Treasure Hunt to pick up a free sticker."
     }
   },
   {
-    tier: 'gold',
-    name: 'Old Republic Title',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'this is what democracy looks like',
-      description: 'write a letter to an elected official encouraging them to fund homeless services'
+    "tier": "gold",
+    "name": "Spaces Realty",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Explore Aqueous at Campbell Art Park"
     }
   },
   {
-    tier: 'gold',
-    name: 'Supreme Lending',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Capitol Hill hello',
-      description: 'Take a selfie outside of 1984 studios, post it to your story, take a screenshot'
+    "tier": "gold",
+    "name": "The Atkinson Team",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Beth Atkinson encurages you to perform a Random Act of Kindness."
     }
   },
   {
-    tier: 'gold',
-    name: 'Cafe 501',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'curbside collab',
-      description: 'buy a curbside chronicle, take a selfie'
+    "tier": "gold",
+    "name": "Oklahoma Fidelity",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Check out one of our local favorites, Georges Happy Hog Bar-B-Q, on 23rd street between 11-7pm on Saturday."
     }
   },
   {
-    tier: 'silver',
-    name: "Eileen's Cookies",
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'all aboard',
-      description: 'take a selfie featuring the train depot mural'
+    "tier": "silver",
+    "name": "Center for Economic Development Law",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Confirm that you are registered to Vote (or help a friend!)"
     }
   },
   {
-    tier: 'silver',
-    name: 'Hefner Grill',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'pick me up',
-      description: 'write an encouraging letter to a homeless youth, drop it off at sisu'
+    "tier": "silver",
+    "name": "Heartland Historical Properties",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Post a selfie in front of HHP's sign at 3503 Oakhurst Drive in MWC - RIDDLE in window?"
     }
   },
   {
-    tier: 'silver',
-    name: 'OCD Cleaning',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'dont be an ass',
-      description: 'like the mule on facebook, take a screenshot'
+    "tier": "silver",
+    "name": "Peace of Mind Pediatrics",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Peace of Mind Pediatrics encourages you to enjoy a walk around Scissortail Park and snap a photo of your favorite part!"
     }
   },
   {
-    tier: 'silver',
-    name: 'Rustic Cuff',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Do the okie pokie',
-      description: 'do the hokey pokey on the steps of the state capital, post the video to your story, tag sisu'
+    "tier": "silver",
+    "name": "Omega Investments",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Explore Eastside OKC public art at the With Love event on Saturday from 4-7pm or submit your favorite East OKC mural anytime! www.instagram.com/p/CFSCEzgjs9I/"
     }
   },
   {
-    tier: 'silver',
-    name: 'Salata',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'stronger together',
-      description: 'buy a sisu stronger together shirt'
+    "tier": "silver",
+    "name": "Freedom Oklahoma",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Freedom Oklahoma invites you to explore an exhibition showcasing local artists' interpretation of the theme UNIFIED at DNA Galleries in the Plaza. Submit the piece that impacts you the most."
     }
   },
   {
-    tier: 'silver',
-    name: 'Lifetime Athletics',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'captain underpants',
-      description: 'bring new underwear to sisu, socks count, no tighty whities'
+    "tier": "silver",
+    "name": "James Cooper",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Write a letter to an elected official encouraging them to fund homeless services."
     }
   },
   {
-    tier: 'silver',
-    name: '3Sixty',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: "Now it's time to get funky",
-      description: 'take a selfie at factory obscura mix tape'
+    "tier": "silver",
+    "name": "Brandon - TOR",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": ""
     }
   },
   {
-    tier: 'silver',
-    name: "Aunti Em's Sweets",
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'wanderlust',
-      description: 'check out aqueous at oklahoma contemporary, take a selfie'
+    "tier": "silver",
+    "name": "First American Title",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Selfie at a first american title - guessing game in window?"
     }
   },
   {
-    tier: 'bronze',
-    name: 'Barking Dog Bakery',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'winter is coming',
-      description: 'bring a new or lightly used coat to sisu'
+    "tier": "bronze",
+    "name": "Sasquatch",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Visit the Sasquatch Shaved Ice stand in Plaza for a free snow cone!"
     }
   },
   {
-    tier: 'bronze',
-    name: 'CMG Financial - Lisa Pape',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Slide to the left',
-      description: ''
+    "tier": "bronze",
+    "name": "Factory Obscura",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Take a selfie at Factory Obscura mix tape. Take advantage of BOGO tickets with the code FOLOVESSISU"
     }
   },
   {
-    tier: 'bronze',
-    name: 'Elliot Roofing',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Slide to the right',
-      description: ''
+    "tier": "bronze",
+    "name": "Curbside",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Buy a Curbside Chronicle and submit a photo of your favorite article."
     }
   },
   {
-    tier: 'bronze',
-    name: 'Embassy Suites - Norman',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Criss cross',
-      description: ''
+    "tier": "bronze",
+    "name": "Holey Rollers",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Stop in at Holey Rollers in the Paseo to write an encouraging note to a homeless youth or Sisu staff member. Show your map to get a free drip coffee or $2 off any drink when you buy a donut!"
     }
   },
   {
-    tier: 'bronze',
-    name: 'Firebirds - OKC',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Cha cha now yall',
-      description: ''
+    "tier": "bronze",
+    "name": "1984 Studios",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Take a selfie outside of 1984 studios! Post it to your story and send us a screenshot."
     }
   },
   {
-    tier: 'bronze',
-    name: 'Jan Wysocki - personal donation',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Hands on your knees',
-      description: ''
+    "tier": "bronze",
+    "name": "Meridian Market",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Drop off new or gently used coats, jackets, etc. at Meridian Market. Show your treasure map here to recieve a FREE craft coffee (or specialty mocktail lemonade) with purchase of an entree."
     }
   },
   {
-    tier: 'bronze',
-    name: 'Jana Thompson',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Reverse, reverse',
-      description: ''
+    "tier": "bronze",
+    "name": "Core4",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Stop in at Core4 Brewery on Film Row to enjoy the patio weather. Show your Treasure Map and get $2 off a beer flight."
     }
   },
   {
-    tier: 'bronze',
-    name: 'Kendra Scott',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Charlie brown',
-      description: ''
+    "tier": "bronze",
+    "name": "Loaded Bowl",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": ""
     }
   },
   {
-    tier: 'bronze',
-    name: 'Nothing Bundt Cakes',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Two hops now yall',
-      description: ''
+    "tier": "bronze",
+    "name": "Revolucion",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Check in at Revolucion for their grand re-opening! Show your Treasure Map to snag a free taco!"
     }
   },
   {
-    tier: 'bronze',
-    name: 'Oklahoma Fidelity Bank',
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Hands on your knees',
-      description: ''
+    "tier": "bronze",
+    "name": "Red Rooster",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "Eat local! Visit during brunch, happy hur, or any time in between and get 15% off all food with the purchase of a cocktail or wine."
     }
   },
   {
-    tier: 'bronze',
-    name: "Packard's",
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: "Drop it like it's hot",
-      description: ''
+    "tier": "bronze",
+    "name": "Dig It",
+    "logo": "https://picsum.photos/60/90",
+    "activity": {
+      "description": "window selfie in your favorite sunnies"
     }
-  },
-  {
-    tier: 'bronze',
-    name: "Rudy's BBQ",
-    logo: 'https://picsum.photos/60/90',
-    activity: {
-      title: 'Pop lock and drop it',
-      description: ''
-    }
-  },
+  }
 ].map(sponsor => ({
   ...sponsor,
   slug: dasherize(sponsor.name),
