fe12eaacc8fb18b93784c555056bf46d12c8e097
allthesignals <wilburnforce@gmail.com>
2020-10-02 13:25:48 -0400

    Hide new board

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -3,7 +3,7 @@ import Controller from '@ember/controller';
 export default class ApplicationController extends Controller {
   queryParams = ['user'];
 
-
+  isLocal = window.location.host.includes('localhost');
 
   user = 'unknown';
 }
